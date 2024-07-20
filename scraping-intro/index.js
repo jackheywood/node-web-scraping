24ab5b759289eeea0f5e361dd3a654b01f4c6be4
Jack Heywood <jackaheywood@gmail.com>
2024-07-20 17:38:40 +0100

    Id, class and attribute selectors

--- scraping-intro/index.js
+++ scraping-intro/index.js
@@ -1,17 +1,47 @@
-const fs = require('fs');
 const cheerio = require('cheerio');
 
-const url = 'https://reactnativetutorial.net/lesson2.html';
-
 async function main() {
-    const html = await fetch(url).then((res) => res.text());
-    fs.writeFileSync('./test.html', html);
+    await selectMultiple();
+    await selectId();
+    await selectClass();
+    await selectAttribute();
+}
 
-    const $ = await cheerio.load(html);
+async function selectMultiple() {
+    console.log('\nSELECT MULTIPLE');
+    const $ = await getAsync('https://reactnativetutorial.net/lesson2.html');
 
-    $('h2').each((index, element) => {
+    $('h2').each((_, element) => {
         console.log($(element).text());
     });
+}
+
+async function selectId() {
+    console.log('\nSELECT ID');
+    const $ = await getAsync('https://reactnativetutorial.net/lesson3.html');
+
+    console.log($('#red').text());
+}
+
+async function selectClass() {
+    console.log('\nSELECT CLASS');
+    const $ = await getAsync('https://reactnativetutorial.net/lesson5.html');
+
+    $('.red').each((_, element) => {
+        console.log($(element).text());
+    });
+}
+
+async function selectAttribute() {
+    console.log('\nSELECT ATTRIBUTE');
+    const $ = await getAsync('https://reactnativetutorial.net/lesson6.html');
+
+    console.log($('[data-customer="22293"]').text());
+}
+
+async function getAsync(url) {
+    const html = await fetch(url).then((res) => res.text());
+    return cheerio.load(html);
 }
 
 // noinspection JSIgnoredPromiseFromCall
