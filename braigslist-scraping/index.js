a48bf0c882ec7f4ba248771dba63617306608e76
Jack Heywood <jackaheywood@gmail.com>
2024-07-20 22:45:49 +0100

    Scrape descriptions (badly)

--- braigslist-scraping/index.js
+++ braigslist-scraping/index.js
@@ -1,21 +1,50 @@
+const axios = require('axios');
 const cheerio = require('cheerio');
 
-const braigslistUrl = 'https://braigslist.vercel.app/jobs/1/';
+const braigslistUrl = 'https://braigslist.vercel.app';
 
-async function main() {
-  const jobIndexPage = await fetch(braigslistUrl).then(res => res.text());
-  const $ = cheerio.load(jobIndexPage);
+async function scrapeJobsFromIndexPages() {
+  const jobs = [];
 
-  const titleLinks = $('.title-blob > a');
+  for (let i = 1; i <= 14; i++) {
+    const jobPageUrl = `${braigslistUrl}/jobs/${i}/`;
+    const jobPage = await axios.get(jobPageUrl);
+    const $ = cheerio.load(jobPage.data);
 
-  const jobs = titleLinks.map((index, link) => {
-    const title = $(link).text();
-    const url = $(link).attr('href');
-    return { title, url };
-  }).get();
+    const titleLinks = $('.title-blob > a');
 
-  console.log(jobs);
+    const pageJobs = titleLinks.map((index, link) => {
+      const title = $(link).text();
+      const url = $(link).attr('href');
+      return { title, url };
+    }).get();
+
+    jobs.push(...pageJobs);
+  }
+
+  return jobs;
 }
 
-// noinspection JSIgnoredPromiseFromCall
-main();
+async function scrapeJobDescriptions(jobs) {
+  // Firing off all requests at once is not a good idea
+  // Uses up all the ports and is likely to get you blocked
+  return await Promise.all(
+    jobs.map(async job => {
+      const descriptionPageUrl = `${braigslistUrl}${job.url}`;
+      const descriptionPage = await axios.get(descriptionPageUrl);
+
+      const $ = cheerio.load(descriptionPage.data);
+
+      job.description = $('div').text();
+      return job;
+    }));
+}
+
+async function main() {
+  return await scrapeJobsFromIndexPages()
+    .then(jobs => scrapeJobDescriptions(jobs));
+}
+
+main().then(jobs => {
+  console.log(jobs);
+});
