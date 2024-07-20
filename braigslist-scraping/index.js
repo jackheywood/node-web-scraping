4dda5593eca8992ecfb0bcc48a3b4c9221ec5a40
Jack Heywood <jackaheywood@gmail.com>
2024-07-20 22:13:59 +0100

    Scrape urls

--- braigslist-scraping/index.js
+++ braigslist-scraping/index.js
@@ -6,9 +6,15 @@ async function main() {
   const jobIndexPage = await fetch(braigslistUrl).then(res => res.text());
   const $ = cheerio.load(jobIndexPage);
 
-  $('.title-blob').each((index, title) => {
-    console.log($(title).text());
-  });
+  const titleLinks = $('.title-blob > a');
+
+  const jobs = titleLinks.map((index, link) => {
+    const title = $(link).text();
+    const url = $(link).attr('href');
+    return { title, url };
+  }).get();
+
+  console.log(jobs);
 }
 
 // noinspection JSIgnoredPromiseFromCall
