1d7f5ecea4098da457af8af2e7f012e7b0a1dc5e
Jack Heywood <jackaheywood@gmail.com>
2024-07-20 18:15:10 +0100

    Select individual cells

--- table-scraping/index.js
+++ table-scraping/index.js
@@ -6,8 +6,16 @@ async function main() {
   const http = await fetch(tableUrl).then(res => res.text());
   const $ = cheerio.load(http);
 
-  $('table > tbody > tr > td').each((_, el) => {
-    console.log($(el).text());
+  $('table > tbody > tr').each((index, row) => {
+    const cells = $(row).find('td');
+
+    const company = $(cells[0]).text();
+    const contact = $(cells[1]).text();
+    const country = $(cells[2]).text();
+
+    console.log('Company:', company);
+    console.log('Contact:', contact);
+    console.log('Country:', country);
   });
 }
 
