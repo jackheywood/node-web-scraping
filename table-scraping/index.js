8ac274264e59bee029b2d610ee81ff9f6f0f6730
Jack Heywood <jackaheywood@gmail.com>
2024-07-20 18:18:41 +0100

    Scrape data into an array

--- table-scraping/index.js
+++ table-scraping/index.js
@@ -6,17 +6,21 @@ async function main() {
   const http = await fetch(tableUrl).then(res => res.text());
   const $ = cheerio.load(http);
 
+  const scrapedData = [];
+
   $('table > tbody > tr').each((index, row) => {
+    if (index === 0) return;
+
     const cells = $(row).find('td');
 
     const company = $(cells[0]).text();
     const contact = $(cells[1]).text();
     const country = $(cells[2]).text();
 
-    console.log('Company:', company);
-    console.log('Contact:', contact);
-    console.log('Country:', country);
+    scrapedData.push({ company, contact, country });
   });
+
+  console.log(scrapedData);
 }
 
 // noinspection JSIgnoredPromiseFromCall
